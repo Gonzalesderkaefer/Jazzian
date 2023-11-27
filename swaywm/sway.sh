b4d4f1c5fc2c4b966c4b12fa91ecb00fef55bbd7
Gonzalesderkaefer <149519890+Gonzalesderkaefer@users.noreply.github.com>
2023-11-27 20:09:53 +0100

    added new swaywm config folder

---
+++ swaywm/sway.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#checking and applying updates
+sudo apt update && sudo apt upgrade -y
+
+#installing packages
+sudo apt install rofi file-roller atril flatpak cbatticon network-manager network-manager-gnome network-manager-openconnect-gnome network-manager-openconnect lxappearance git snapd ranger neovim fonts-jetbrains-mono firefox-esr tlp alacritty brightnessctl pulsemixer sway waybar fonts-material-design-icons-iconfont fonts-font-awesome 
+
+#special packages
+sudo apt --no-install-recommends sddm
+
+
+#copying config files
+ranger --copy-config=all
+cp -r alacritty/ $HOME/.config/alacritty/
+cp -r rofi/ $HOME/.config/
+cp -r sway $HOME/.config/
+sudo cp -r waybar/ /etc/xdg/
+
+#systemd
+sudo systemctl enable tlp
+sudo systemctl enable sddm
+sudo systemctl set-default graphical.target
