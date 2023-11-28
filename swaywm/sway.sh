00fadb8bce1d0323d132fd5a0114ed71e45f61f7
Gonzalesderkaefer <149519890+Gonzalesderkaefer@users.noreply.github.com>
2023-11-28 11:36:44 +0100

    Update sway.sh

--- swaywm/sway.sh
+++ swaywm/sway.sh
@@ -6,9 +6,6 @@ sudo apt update && sudo apt upgrade -y
 #installing packages
 sudo apt install rofi file-roller atril flatpak cbatticon network-manager network-manager-gnome network-manager-openconnect-gnome network-manager-openconnect lxappearance git snapd ranger neovim fonts-jetbrains-mono firefox-esr tlp alacritty brightnessctl pulsemixer sway waybar fonts-material-design-icons-iconfont fonts-font-awesome xwayland libglib2.0-bin
 
-#special packages
-sudo apt --no-install-recommends sddm
-
 
 #copying config files
 ranger --copy-config=all
