e2761da89b0c18e5a92a18a37183f6c7bc2ba456
gonzalesderkaefer <gonzalesderkaefer@github.com>
2023-12-19 12:43:59 +0100

    set evince as default application

--- script.sh
+++ script.sh
@@ -4,7 +4,9 @@
 sudo apt update && sudo apt upgrade -y
 
 #installing packages
-sudo apt install bspwm sxhkd lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings nitrogen rofi picom file-roller atril flatpak polybar cbatticon network-manager network-manager-gnome network-manager-openconnect-gnome network-manager-openconnect lxappearance git snapd neovim xorg fonts-jetbrains-mono firefox-esr tlp alacritty brightnessctl pulsemixer xinput gnome-themes-extra arandr fonts-material-design-icons-iconfont fonts-font-awesome lf
+sudo apt install bspwm sxhkd lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings nitrogen rofi picom file-roller evince flatpak polybar cbatticon network-manager network-manager-gnome network-manager-openconnect-gnome network-manager-openconnect lxappearance git snapd neovim xorg fonts-jetbrains-mono firefox-esr tlp alacritty brightnessctl pulsemixer xinput gnome-themes-extra arandr fonts-material-design-icons-iconfont fonts-font-awesome lf
+#setting default applications
+xdg-mime default org.gnome.Evince.desktop application/pdf
 
 #copying config files
 ranger --copy-config=all
