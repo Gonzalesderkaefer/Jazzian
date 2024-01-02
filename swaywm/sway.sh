24ef2edf867942dcb3c0555045b7dd8a05958e1b
gonzalesderkaefer <gonzalesderkaefer>
2024-01-02 08:34:10 +0100

    added wl-clipboard to sway.sh

--- swaywm/sway.sh
+++ swaywm/sway.sh
@@ -4,7 +4,7 @@
 sudo apt update && sudo apt upgrade -y
 
 #installing packages
-sudo apt install rofi file-roller atril flatpak cbatticon network-manager network-manager-gnome network-manager-openconnect-gnome network-manager-openconnect lxappearance git snapd ranger neovim fonts-jetbrains-mono firefox-esr tlp alacritty brightnessctl pulsemixer sway waybar fonts-material-design-icons-iconfont fonts-font-awesome xwayland libglib2.0-bin fonts-noto-color-emoji wlr-randr
+sudo apt install rofi file-roller evince flatpak cbatticon network-manager network-manager-gnome network-manager-openconnect-gnome network-manager-openconnect lxappearance git snapd lf neovim fonts-jetbrains-mono firefox-esr tlp alacritty brightnessctl pulsemixer sway waybar fonts-material-design-icons-iconfont fonts-font-awesome xwayland libglib2.0-bin fonts-noto-color-emoji wlr-randr nala wl-clipboard
 
 
 #copying config files
@@ -17,10 +17,3 @@ sudo cp -r waybar/ /etc/xdg/
 #systemd
 sudo systemctl enable tlp
 sudo systemctl set-default graphical.target
-
-#shell_configuration
-echo 'exec sway' >> .profile
-echo 'alias "ll"="ls -la"' >> $(echo $SHELL | awk -F '/' '{print $3}').rc
-echo 'alias "sn"="sudo nala"' >> $(echo $SHELL | awk -F '/' '{print $3}').rc
-echo 'alias "n"="nala"' >> $(echo $SHELL | awk -F '/' '{print $3}').rc
-
