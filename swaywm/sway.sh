3721efb3a7bae248bd92adb02c43d3227c247aa7
gonzalesderkaefer <gonzalesderkaefer>
2024-01-04 07:55:39 +0100

    added Monitor Check script

--- swaywm/sway.sh
+++ swaywm/sway.sh
@@ -6,7 +6,13 @@ sudo apt update && sudo apt upgrade -y
 #installing packages
 sudo apt install rofi file-roller evince flatpak cbatticon network-manager network-manager-gnome network-manager-openconnect-gnome network-manager-openconnect lxappearance git snapd lf neovim fonts-jetbrains-mono firefox-esr tlp alacritty brightnessctl pulsemixer sway waybar fonts-material-design-icons-iconfont fonts-font-awesome xwayland libglib2.0-bin fonts-noto-color-emoji wlr-randr nala wl-clipboard
 
+#copying scripts to .local/bin
 
+        if [ -n "$(ls $HOME/.local/ | grep -o bin)" ] #make directory if .local/bin does not exist
+        then
+        else
+                mkdir $HOME/.local/bin/
+        fi
 #copying config files
 cp -r alacritty/ $HOME/.config/alacritty/
 cp -r rofi/ $HOME/.config/
