5ed961fa067c3e58434b9977ae6c627123722906
gonzalesderkaefer <gonzalesderkaefer>
2024-01-05 14:54:31 +0100

    added random string generator to use with passadd

---
+++ swaywm/passgen/passgen.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <random>
+
+
+int randomNumber()
+{
+    std::random_device random;
+    std::uniform_int_distribution<int> dist (0,83);
+    return dist(random);
+}
+
+int main(){
+   const char characters [84] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+'.', '!', ',', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '/', '\\', '|', '[', ']', '{', '}', '<', '>', '?', ';', ':', '`', '~'
+};
+
+std::string a = "";
+
+    
+
+    int k;
+    
+    std::cout << "How Many characters?" << "\n";
+
+    
+    std::cin >> k; 
+    int i = 0;
+    while (i<k)
+    { 
+        a = a + characters[randomNumber()];
+        ++i;
+    }
+    
+
+    std::cout << a << "\n";
+    return 0;
+}
