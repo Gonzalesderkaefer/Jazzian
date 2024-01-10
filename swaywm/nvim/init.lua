ac54864c13715c102294e4576f83d7ecd08d8169
gonzalesderkaefer <gonzalesderkaefer>
2024-01-10 20:07:51 +0100

    changed nvim tabstop to 4 chars

--- swaywm/nvim/init.lua
+++ swaywm/nvim/init.lua
@@ -1,12 +1,9 @@
 
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-vim.bo.softtabstop = 2
+vim.bo.softtabstop = 4
 
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
 vim.cmd("set number")
 vim.cmd("set cursorline")
 vim.cmd("hi CursorLine term=bold cterm=bold ctermbg=236")
