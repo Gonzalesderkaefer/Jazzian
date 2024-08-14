0aff4671100247f33bed95b2392081034b063fd3
Gonzales Der Kaefer <149519890+Gonzalesderkaefer@users.noreply.github.com>
2024-08-14 21:13:18 +0200

    finalized custom.sh

--- modules/custom.sh
+++ modules/custom.sh
@@ -1,5 +1,11 @@
 #! /bin/bash
 
+## checking if is run with two arguements
+if [ -z $1 ] || [ -z $2 ]; then
+    echo -e "$start_red This script expects at least two arguments $end_red";
+    exit 1;
+fi
+
 # color variables
 start_green="\033[0;32m"  
 end_green="\033[0m "
@@ -8,6 +14,7 @@ end_red="\033[0m"
 
 # variables
 custom=devicespecific;
+custom_theme=devicespecific_theme;
 customrc=devicerc;
 
 echo -e "$start_green Going to create $custom files for custom conifiguration $end_green";
@@ -24,18 +31,18 @@ fi
 if ! [ -f $HOME/.$custom.sh ]; then
     touch $HOME/.$custom.sh;
     
-    echo 'shell_tokill="$(echo $SHELL | grep -E -o "[^\/]*$")"' >> $HOME/.devicespecific.sh
-    echo 'shells=$(ps | grep -E "tty1.*$shell_tokill" | awk '\''{print $1}'\'')' >> $HOME/.devicespecific.sh
+    echo 'shell_tokill="$(echo $SHELL | grep -E -o "[^\/]*$")"' >> $HOME/.$custom.sh
+    echo 'shells=$(ps | grep -E "tty1.*$shell_tokill" | awk '\''{print $1}'\'')' >> $HOME/.$custom.sh
 
 
-    echo 'killshells()' >> $HOME/.devicespecific.sh
+    echo 'killshells()' >> $HOME/.$custom.sh
     echo '{' >> $HOME/.devicespecific.sh
-    echo '   for shell in $shells; do' >> $HOME/.devicespecific.sh
-    echo '       kill -9 $shell;' >> $HOME/.devicespecific.sh
-    echo '   done' >> $HOME/.devicespecific.sh
-    echo '}' >> $HOME/.devicespecific.sh
+    echo '   for shell in $shells; do' >> $HOME/.$custom.sh
+    echo '       kill -9 $shell;' >> $HOME/.$custom.sh
+    echo '   done' >> $HOME/.$custom.sh
+    echo '}' >> $HOME/.$custom.sh
 
-    echo '[ "$(tty)" = "/dev/tty1" ] && (startx; killshells)' >> $HOME/.devicespecific.sh
+    echo '[ "$(tty)" = "/dev/tty1" ] && (startx; killshells)' >> $HOME/.$custom.sh
 fi
 
 # create xinitrc file regardless of display server choice
@@ -46,13 +53,52 @@ fi
 # configure which session needs to be started
 case $1 in
     "wayland")
-        sed -i "s/\&\& (.*; killshells)/\&\& ($2; killshells) /g" $HOME/.devicespecific.sh
+        sed -i "s/\&\& (.*; killshells)/\&\& ($2; killshells) /g" $HOME/.$custom.sh
         ;;
     *)
-        sed -i "s/\&\& (.*; killshells)/\&\& (startx; killshells) /g" $HOME/.devicespecific.sh;
+        sed -i "s/\&\& (.*; killshells)/\&\& (startx; killshells) /g" $HOME/.$custom.sh;
         sed -E -i "s/exec awesome|exec bspwm|exec i3/exec $2/g" $HOME/.xinitrc;
         ;;
 esac
+
+
+# Creating files for i3
+mkdir -p $HOME/.config/i3/$custom
+[ -f $HOME/.config/i3/$custom/$custom ] || touch $HOME/.config/i3/$custom/$custom;
+
+# Creating files for awesomewm
+[ -f $HOME/.config/awesome/$custom.lua ] || touch $HOME/.config/awesome/$custom.lua;
+[ -f $HOME/.config/awesome/$custom_theme.lua ] || touch $HOME/.config/awesome/$custom_theme.lua;
+
+# Creating files for bspwm
+mkdir -p $HOME/.config/bspwm/$custom/;
+[ -f $HOME/.config/bspwm/$custom/$custom ] || ( touch $HOME/.config/bspwm/$custom/$custom;
+echo '#!/bin/sh' >> $HOME/.config/bspwm/$custom/$custom );
+
+# Creating files for Hyprland
+mkdir -p $HOME/.config/hypr/$custom/;
+[ -f $HOME/.config/hypr/$custom/$custom ] || touch $HOME/.config/hypr/$custom/$custom;
+
+
+# Creating files for sway 
+mkdir -p $HOME/.config/sway/$custom/;
+[ -f $HOME/.config/sway/$custom/$custom ] || touch $HOME/.config/sway/$custom/$custom;
+
+# Creating files for river 
+mkdir -p $HOME/.config/river/$custom/;
+[ -f $HOME/.config/river/$custom/$custom ] || touch $HOME/.config/river/$custom/$custom;
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
