9f57772b560779e0630d71662477067f0104faf8
Gonzales Der Kaefer <149519890+Gonzalesderkaefer@users.noreply.github.com>
2024-08-20 07:28:24 +0200

    added nav keys to telescope

--- cfg_files/nvim/lua/plugins/telescope.lua
+++ cfg_files/nvim/lua/plugins/telescope.lua
@@ -4,9 +4,24 @@ return {
       config = function()
         -- Setting telescope keybinds
         local builtin = require('telescope.builtin')
+        local telescope = require("telescope")
+        local actions = require("telescope.actions")
         vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
         vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
         vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
         vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-    end
+        telescope.setup({
+            defaults = {
+                path_display = { "smart" },
+                mappings = {
+                    i = {
+                        ["<C-k>"] = actions.move_selection_previous,
+                        ["<C-j>"] = actions.move_selection_next,
+                        ["<C-q>"] = actions.move_selection_next,
+                    },
+                },
+            }
+        })
+
+    end,
 }
