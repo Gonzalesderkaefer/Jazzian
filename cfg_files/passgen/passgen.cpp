b65804c2c5b9d95d0076ca9e4128572fd642f161
Gonzalesderkaefer <149519890+Gonzalesderkaefer@users.noreply.github.com>
2024-08-26 11:10:31 +0200

    added some output

--- cfg_files/passgen/passgen.cpp
+++ cfg_files/passgen/passgen.cpp
@@ -4,25 +4,26 @@
 #include <random>
 
 
-int randomNumber()
+int randomNumber(int first, int second)
 {
     std::random_device random;
-    std::uniform_int_distribution<int> dist (0,83);
+    std::uniform_int_distribution<int> dist(first,second);
     return dist(random);
 }
 
 int main(){
-   const char characters [84] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-'.', '!', ',', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '/', '\\', '|', '[', ']', '{', '}', '<', '>', '?', ';', ':', '`', '~'
-};
-
- 
+    const char characters[84] = {
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+        'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b',
+        'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+        'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '.', '!', ',', '@',
+        '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '/', '\\',
+        '|', '[', ']', '{', '}', '<', '>', '?', ';', ':', '`', '~'};
 
     std::string a = "";
     int k;
-    std::cin >> k; 
-    int i = 0;
+    std::cout << "How many Chars?" << "\n";
+    std::cin >> k;
 
     if (!std::cin) {
         // input was not an integer
@@ -32,14 +33,14 @@ int main(){
 
 
     for ( int j = 0; j < k; ++j  ){
-        a = a + characters[randomNumber()];
+        a = a + characters[randomNumber(0,83)];
     }
 
-//while (i<k)
-//    { 
-//        a = a + characters[randomNumber()];
-//        ++i;
-//    }
+    //while (i<k)
+    //    { 
+    //        a = a + characters[randomNumber()];
+    //        ++i;
+    //    }
     std::cout << a << "\n";
     return 0;
 }
