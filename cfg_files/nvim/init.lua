dc7d6ea983f2f57b78c09ec1cb9550ae7521c9d0
Gonzales Der Kaefer <149519890+Gonzalesderkaefer@users.noreply.github.com>
2024-10-05 12:51:23 +0200

    checkig for user-specific file works properly now

--- cfg_files/nvim/init.lua
+++ cfg_files/nvim/init.lua
@@ -2,14 +2,14 @@
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath) 
 
@@ -21,14 +21,14 @@ require("nvim-settings")
 -- function to check if file exists
 local file_exists = function (name)
     local f = io.open(name,"r")
-    return f ~= nil and io.close(name)
+    return f ~= nil and io.close(f)
 end
 
 -- include user-specific settings
-if file_exists("$HOME/.config/nvim/lua/devicespecific.lua") then
+if file_exists(os.getenv("HOME") .. "/.config/nvim/lua/devicespecific.lua") then
     require("devicespecific")
 end
 
 -- make lazy install stuff
-require("lazy").setup("plugins") 
+require("lazy").setup("plugins")
 
