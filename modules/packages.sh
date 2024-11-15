fbeb6b7492310672c1df452bd749b4310338ae78
Gonzales Der Kaefer <149519890+Gonzalesderkaefer@users.noreply.github.com>
2024-11-15 19:53:40 +0100

    first draft of packages

--- modules/packages.sh
+++ modules/packages.sh
@@ -4,5 +4,10 @@
 # definitions of the packages for each distro
 
 ## Debian standard packages
-debian_packages="zsh nnn vim git alacritty wofi"
+debian_packages="zsh nnn vim git alacritty wofi acpi-support tmux acpid acpi linux-cpupower cpufrequtils openssh-server fzf mpv";
 
+## Fedora standard packages
+fedora_packages="zsh nnn vim git alacritty wofi acpi acpid tmux mpv kernel-tools openssh openssh-server fzf";
+
+## Arch standart packages
+arch_packages="fzf tmux zsh nnn bash-completion zsh-completions mpv ";
