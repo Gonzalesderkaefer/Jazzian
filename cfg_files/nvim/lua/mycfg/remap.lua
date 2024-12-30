a3006896e8a9e7ced2b6f47a621e3c96f7d42eea
Gonzales Der Kaefer <149519890+Gonzalesderkaefer@users.noreply.github.com>
2024-12-30 05:34:45 +0100

    prompt colors

--- cfg_files/nvim/lua/mycfg/remap.lua
+++ cfg_files/nvim/lua/mycfg/remap.lua
@@ -1,31 +1,30 @@
-
 local v = vim
 
 -- Setting leader to space
 v.g.mapleader = " "
 
 -- Configuring telescope
-v.keymap.set("n", "<leader>ff", function () v.cmd.Telescope("find_files") end)
-v.keymap.set('n', '<leader>fb', ":Telescope buffers<CR>", { desc = 'Telescope buffers' })
-
+v.keymap.set("n", "<leader>ff", function()
+	v.cmd.Telescope("find_files")
+end)
+v.keymap.set("n", "<leader>fb", ":Telescope buffers<CR>", { desc = "Telescope buffers" })
 
 -- Defocus terminal by hitting ESC twice
-v.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>')
+v.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>")
 
 -- Oil
 v.keymap.set("n", "<leader>pv", v.cmd.Oil)
 
--- Launch Terminal 
-v.keymap.set("n", "<F12>", function ()
-    v.cmd.vnew()
-    v.cmd.term()
-    v.cmd.wincmd("J")
-    v.api.nvim_win_set_height(0,20)
+-- Launch Terminal
+v.keymap.set("n", "<F12>", function()
+	v.cmd.vnew()
+	v.cmd.term()
+	v.cmd.wincmd("J")
+	v.api.nvim_win_set_height(0, 20)
 end)
 
 -- Clear search
-v.keymap.set("n", "<leader>nh", v.cmd.nohlsearch)
-
+v.keymap.set("n", "<Esc>", v.cmd.nohlsearch)
 
 -- Splits
 v.keymap.set("n", "<leader>sv", v.cmd.vnew)
@@ -33,15 +32,13 @@ v.keymap.set("n", "<leader>sh", v.cmd.new)
 v.keymap.set("n", "<leader>se", "<C-w>=")
 v.keymap.set("n", "<leader>sx", v.cmd.close)
 
-
 -- Buffer navigation
 v.keymap.set("n", "<leader>nn", v.cmd.bn)
 v.keymap.set("n", "<leader>NN", v.cmd.bprevious)
 v.keymap.set("n", "<leader>qq", v.cmd.bdelete)
 
 -- Pasting without losing initially yanked string
-v.keymap.set("x", "<leader>p", "\"_dP")
-
+v.keymap.set("x", "<leader>p", '"_dP')
 
 -- Move in diagnostics
 v.keymap.set("n", "<leader>dn", v.diagnostic.goto_next)
