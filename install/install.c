9d7e9b3cabdd430dec3efc09e036f7a09d0aa938
Gonzales Der Kaefer <149519890+Gonzalesderkaefer@users.noreply.github.com>
2024-12-31 19:47:05 +0100

    started with regex

--- install/install.c
+++ install/install.c
@@ -1,6 +1,6 @@
 /* Libraries */
-#include <stdio.h>
 #include <regex.h>
+#include <stdio.h>
 
 /* Other files */
 #include "vars.h"
@@ -14,7 +14,7 @@ enum DISPLAYSERVER get_display_server() {
   /* Get users choice */
   char choice = getchar();
 
-  if(choice == 'w' || choice == 'W')
+  if (choice == 'w' || choice == 'W')
     return WAYLAND;
 
   return XORG;
@@ -24,60 +24,55 @@ enum WINDOWMANAGER get_window_manager(enum DISPLAYSERVER display_server) {
   /* Ask user */
   printf("\033[1;35mChoose a Windowmanager\033[0m\n");
   char choice;
-  switch(display_server) {
-    case XORG:
-      printf("\033[0;32m[i]3 (default)\033[0m\n");
-      printf("\033[0;32m[a]wesome \033[0m\n");
-      printf("\033[0;32m[b]spwm \033[0m\n");
-      printf("Your Choice: ");
+  switch (display_server) {
+  case XORG:
+    printf("\033[0;32m[i]3 (default)\033[0m\n");
+    printf("\033[0;32m[a]wesome \033[0m\n");
+    printf("\033[0;32m[b]spwm \033[0m\n");
+    printf("Your Choice: ");
 
-      /* Get users choice */
-      choice = getchar();
+    /* Get users choice */
+    choice = getchar();
 
-      /* Check user choice and return */
-      if(choice == 'b' || choice == 'B') {
-        return BSPWM;
-      } else if (choice == 'a' || choice == 'A') {
-        return AWESOME;
-      } else {
-        return I3;
-      }
+    /* Check user choice and return */
+    if (choice == 'b' || choice == 'B') {
+      return BSPWM;
+    } else if (choice == 'a' || choice == 'A') {
+      return AWESOME;
+    } else {
+      return I3;
+    }
 
-      break;
-    case WAYLAND:
-      printf("\033[0;32m[s]way (default)\033[0m\n");
-      printf("\033[0;32m[h]yprland \033[0m\n");
-      printf("\033[0;32m[r]iver \033[0m\n");
-      printf("Your Choice: ");
+    break;
+  case WAYLAND:
+    printf("\033[0;32m[s]way (default)\033[0m\n");
+    printf("\033[0;32m[h]yprland \033[0m\n");
+    printf("\033[0;32m[r]iver \033[0m\n");
+    printf("Your Choice: ");
 
+    /* Get users choice */
+    choice = getchar();
 
-      /* Get users choice */
-      choice = getchar();
+    /* Check user choice and return */
+    if (choice == 'h' || choice == 'H') {
+      return HYPRLAND;
+    } else if (choice == 'r' || choice == 'R') {
+      return RIVER;
+    } else {
+      return SWAY;
+    }
 
-      /* Check user choice and return */
-      if(choice == 'h' || choice == 'H') {
-        return HYPRLAND;
-      } else if (choice == 'r' || choice == 'R') {
-        return RIVER;
-      } else {
-        return SWAY;
-      }
-
-
-      break;
+    break;
   }
 }
 
 enum DISTRO get_distro() {
-  FILE *fd = fopen("/etc/os-release","r");
-  if(!fd) {
+  FILE *fd = fopen("/etc/os-release", "r");
+  if (!fd) {
     fprintf(stderr, "Could not open release file");
     return UNKNOWN;
   }
-
 }
-
-
 
 int main() {
   config this_config;
@@ -85,7 +80,30 @@ int main() {
   this_config.display_manager = get_display_server();
   /* getchar won't work otherwise */
   int c;
-  while ((c = getchar()) != '\n' && c != EOF);
+  while ((c = getchar()) != '\n' && c != EOF)
+    ;
   /* get window manager */
   this_config.window_manager = get_window_manager(this_config.display_manager);
+
+
+
+
+
+
+  regex_t new_regex;
+  regmatch_t pmatch;
+
+  regcomp(&new_regex, "Debian|debian|DEBIAN",REG_EXTENDED);
+  int result = regexec(&new_regex, "Debain LInux",new_regex.re_nsub, &pmatch, REG_NOTEOL);
+  printf("%d\n",result);
+  regfree(&new_regex);
+
+
+
+
+
+
+
+
+
 }
