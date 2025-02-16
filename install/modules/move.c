2e5cc32555fe213ac2dad9a8ca10ed23d9701069
Gonzales Der Kaefer <149519890+Gonzalesderkaefer@users.noreply.github.com>
2025-02-16 17:58:19 +0000

    move scripts

--- install/modules/move.c
+++ install/modules/move.c
@@ -8,6 +8,13 @@ int link_cfg() {
   strcpy(cfg_src,getenv("HOME"));
   strcat(cfg_src,"/Jazzian/cfg_files");
 
+
+  // Define script directory
+  int binsrc_len = strlen(getenv("HOME")) + strlen("/Jazzian/bin") + 1;
+  char binsrc[cfg_src_len];
+  strcpy(binsrc,getenv("HOME"));
+  strcat(binsrc,"/Jazzian/bin");
+
   // Define config dest directory
   int cfg_dest_len = strlen(getenv("HOME")) + strlen("/.config") + 1;
   char cfg_dest[cfg_src_len];
@@ -62,26 +69,43 @@ int link_cfg() {
 
 
 
+  // Link general config files
   DIR *cfg_files = opendir(cfg_src);
   struct dirent *cfg_content;
   while ((cfg_content = readdir(cfg_files)) != NULL) {
     // Check if dir name is in ill_cfg
     for(int i = 0; ill_cfg[i] != NULL; ++i){
       if (!strcmp(ill_cfg[i], cfg_content->d_name)) 
-        goto cont;
+        goto contcfg;
     }
     printf("linking %s/%s to %s/%s\n",cfg_src, cfg_content->d_name, cfg_dest, cfg_content->d_name);
 
-
-  cont:
+  contcfg:
     continue;
   }
-
-
-
-
-
   closedir(cfg_files);
+
+  // Link scripts
+  DIR *scripts = opendir(binsrc);
+  struct dirent *script;
+  while ((script = readdir(scripts)) != NULL) {
+    // Check if dir name is in ill_cfg
+    for(int i = 0; ill_cfg[i] != NULL; ++i){
+      if (!strcmp(ill_cfg[i], script->d_name)) 
+        goto contscript;
+    }
+    printf("linking %s/%s to %s/%s\n",binsrc, script->d_name, localbin, script->d_name);
+
+  contscript:
+    continue;
+  }
+  closedir(scripts);
+
+
+
+
+
+
   return 0;
 }
 
