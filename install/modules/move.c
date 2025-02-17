7ef6679e9834738d8d338dd8f57cccfdf0a37221
Gonzales Der Kaefer <149519890+Gonzalesderkaefer@users.noreply.github.com>
2025-02-17 06:04:03 +0000

    building full file paths

--- install/modules/move.c
+++ install/modules/move.c
@@ -2,6 +2,8 @@
 #include "../def.h"
 #include <dirent.h>
 #include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
 
 
 
@@ -9,19 +11,42 @@
 int link_dir(char *src_dir, char *dest_dir, char **ill_cfg, bool hide){
   DIR *directory = opendir(src_dir);
   struct dirent *cfg_content;
-  while ((cfg_content = readdir(directory)) != NULL) {
+outside : while ((cfg_content = readdir(directory)) != NULL) {
     // Check if dir name is in ill_cfg
     for(int i = 0; ill_cfg[i] != NULL; ++i){
       if (!strcmp(ill_cfg[i], cfg_content->d_name)) 
-        goto contcfg;
+        goto outside;
     }
-    if (hide)
-      printf("linking %s/%s to %s/.%s\n",src_dir, cfg_content->d_name, dest_dir, cfg_content->d_name);
-    else
-      printf("linking %s/%s to %s/%s\n",src_dir, cfg_content->d_name, dest_dir, cfg_content->d_name);
 
-  contcfg:
-    continue;
+    uint direlem_len = strlen(cfg_content->d_name);
+    uint src_len = strlen(src_dir);
+    uint dest_len = strlen(dest_dir);
+
+    // Define src file path
+    char src[src_len + 1 + direlem_len + 1];
+    strcpy(src, src_dir);
+    strcat(src, "/");
+    strcat(src, cfg_content->d_name);
+    src[src_len + 1 + direlem_len] = '\0';
+
+
+    if (hide) {
+      // Define dest hidden file path
+      char dest[dest_len + 2 + direlem_len + 1];
+      strcpy(dest, dest_dir);
+      strcat(dest, "/.");
+      strcat(dest, cfg_content->d_name);
+      dest[dest_len + 2 + direlem_len] = '\0';
+      printf("linking %s to %s\n", src, dest);
+    } else {
+      // Define dest file path
+      char dest[dest_len + 1 + direlem_len + 1];
+      strcpy(dest, dest_dir);
+      strcat(dest, "/");
+      strcat(dest, cfg_content->d_name);
+      printf("linking %s to %s\n", src, dest);
+    }
+
   }
   closedir(directory);
   return 0;
