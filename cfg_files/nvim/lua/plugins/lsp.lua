b3d7fa7dfa700345c44f1730d491557ae95b37c7
Gonzales Der Kaefer <149519890+Gonzalesderkaefer@users.noreply.github.com>
2025-03-19 14:56:35 +0100

    added Mason

--- cfg_files/nvim/lua/plugins/lsp.lua
+++ cfg_files/nvim/lua/plugins/lsp.lua
@@ -17,6 +17,36 @@ return {
                 cmd = { "clangd" },
                 filetypes = { "c", "cc", "h", "hh" },
             }
+
+        end,
+    },
+
+
+
+
+    -- Mason
+    {
+        "williamboman/mason.nvim",
+    },
+
+    -- Mason lspconfig
+    {
+        "williamboman/mason-lspconfig.nvim",
+
+        dependencies = {
+            "williamboman/mason.nvim",
+            "neovim/nvim-lspconfig",
+        },
+        config = function()
+            require("mason").setup()
+            require("mason-lspconfig").setup()
+
+            -- Setup LSPs installed from Mason automatically
+            require("mason-lspconfig").setup_handlers {
+                function (server_name) -- default handler (optional)
+                    require("lspconfig")[server_name].setup {}
+                end,
+            }
         end,
     },
 }
