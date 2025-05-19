7297c74ffe7fa76c822b8b46b9570deaf5de38d9
Gonzalesderkaefer <149519890+Gonzalesderkaefer@users.noreply.github.com>
2025-05-19 13:40:31 +0200

    removed setup handlers

--- cfg_files/nvim/lua/plugins/lsp.lua
+++ cfg_files/nvim/lua/plugins/lsp.lua
@@ -29,11 +29,11 @@ return {
             require("mason-lspconfig").setup()
 
             -- Setup LSPs installed from Mason automatically
-            require("mason-lspconfig").setup_handlers {
-                function (server_name) -- default handler (optional)
-                    require("lspconfig")[server_name].setup {}
-                end,
-            }
+            -- require("mason-lspconfig").setup_handlers {
+            --     function (server_name) -- default handler (optional)
+            --         require("lspconfig")[server_name].setup {}
+            --     end,
+            -- }
         end,
     },
 }
