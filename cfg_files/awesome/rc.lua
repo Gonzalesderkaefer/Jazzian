27ec267be82ad6d49b88a84981a40fc94a2ad54b
Gonzales Der Kaefer <149519890+Gonzalesderkaefer@users.noreply.github.com>
2025-05-24 08:35:13 +0200

    Awesome all monitors get all desktops

--- cfg_files/awesome/rc.lua
+++ cfg_files/awesome/rc.lua
@@ -400,10 +400,8 @@ awful.screen.connect_for_each_screen(function(s)
     -- Wallpaper
     set_wallpaper(s)
 
-    if s.index == 1 then
-        -- Each screen has its own tag table.
-        awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9" }, s, awful.layout.layouts[1])
-    end
+    -- Each screen has its own tag table.
+    awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9" }, s, awful.layout.layouts[1])
 
 
     -- Create a promptbox for each screen
