1f7ebd32a8a8a1ba7683bc496462dfb33184f1cd
Gonzales Der Kaefer <149519890+Gonzalesderkaefer@users.noreply.github.com>
2025-06-04 19:29:19 +0200

    Using default theme in NVIM

--- cfg_files/nvim/lua/plugins/theme.lua
+++ cfg_files/nvim/lua/plugins/theme.lua
@@ -4,7 +4,7 @@ return {
         priority = 1000, -- Make sure to load this before all the other start plugins.
         init = function()
             vim.cmd.hi 'Comment gui=none'
-            vim.cmd.colorscheme 'onedark'
+            vim.cmd.colorscheme 'default'
         end,
     },
 }
