7d954114c3c862ccc2761470107c7c32772cce2e
Gonzales Der Kaefer <149519890+Gonzalesderkaefer@users.noreply.github.com>
2025-07-05 16:26:15 +0200

    removed old devicespecific files

--- dotfiles/config/hypr/devicespecific/set_monitor.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-
-wlr-randr --output DP-2 --off && (sleep 1 & hyprctl dispatch workspace 1)  && wlr-randr --output DP-2 --on --pos 2560,-250 --transform 90;
--- dotfiles/config/hypr/devicespecific/wallpaper.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-hyprctl hyprpaper preload "~/M_et_D/Media/Images/s6kdo90fh6oa1.jpg"
-hyprctl hyprpaper wallpaper ",~/M_et_D/Media/Images/s6kdo90fh6oa1.jpg"
