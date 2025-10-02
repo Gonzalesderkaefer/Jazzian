2a3cf3a7d9a203e9654667e79acd6a1fca8e04cb
Gonzales Der Kaefer <149519890+Gonzalesderkaefer@users.noreply.github.com>
2025-10-02 09:13:24 +0200

    vimwiki config

--- dotfiles/config/nvim/lua/plugins/vimwiki.lua
+++ dotfiles/config/nvim/lua/plugins/vimwiki.lua
@@ -1,1 +1,14 @@
-return { "vimwiki/vimwiki" }
+return { 
+    "vimwiki/vimwiki",
+
+    init = function()
+        local list = {}
+        list.path = "~/Documents/notes"
+        list.syntax = "markdown"
+        list.ext = ".md"
+        vim.g.vimwiki_list = {
+            list,
+        }
+
+    end
+}
