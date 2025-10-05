89bd92029dcef471a8ac2bb07d5f098891c26873
Gonzales Der Kaefer <149519890+Gonzalesderkaefer@users.noreply.github.com>
2025-10-05 15:15:51 +0200

    added new nvim config

---
+++ dotfiles/config/nvim_minimal/nvim/lua/plugins/theme.lua
@@ -0,0 +1,18 @@
+return {
+    {
+        'navarasu/onedark.nvim',
+        priority = 1000, -- Make sure to load this before all the other start plugins.
+    },
+    {
+        'folke/tokyonight.nvim',
+        priority = 1000, -- Make sure to load this before all the other start plugins.
+        init = function()
+            vim.cmd.colorscheme 'onedark'
+            vim.cmd(":highlight StatusLine guibg=NONE")
+        end,
+    },
+    {
+        "rebelot/kanagawa.nvim",
+        priority = 1000
+    },
+}
