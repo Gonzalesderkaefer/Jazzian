d854f5f810f3f6c126b3d2e0419b02d11eafb316
Gonzalesderkaefer <149519890+Gonzalesderkaefer@users.noreply.github.com>
2026-03-03 09:24:18 +0100

    Should not error now

--- dotfiles/config/nvim/lua/plugins/treesitter.lua
+++ dotfiles/config/nvim/lua/plugins/treesitter.lua
@@ -4,5 +4,16 @@ return
     lazy = false,
     build = ':TSUpdate',
     init = function()
+        vim.api.nvim_create_autocmd("FileType", {
+            group = vim.api.nvim_create_augroup("tree-sitter-enable", { clear = true }),
+            callback = function(args)
+                local lang = vim.treesitter.language.get_lang(args.match)
+                if not lang then return end
+                local ok, query = pcall(vim.treesitter.query.get, lang, "highlights")
+                if ok and query then
+                    vim.treesitter.start()
+                end
+            end,
+        })
     end
 }
